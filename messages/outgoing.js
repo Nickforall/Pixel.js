f534c67ee898be8ce998f3f78f907a67a00ba4f2
Nickforall <hello@nickforall.nl>
2017-07-04 21:58:43 +0200

    FIX: wrong packet header for PlayerDataComposer

--- messages/outgoing.js
+++ messages/outgoing.js
@@ -1,7 +1,7 @@
 module.exports = {
     MachineIdComposer: 1488,
     PlayerAuthOkComposer: 2491,
-    PlayerDataComposer: 3920,
+    PlayerDataComposer: 2725,
     PlayerPerksComposer: 2586,
     PlayerHomeComposer: 2875,
 };
